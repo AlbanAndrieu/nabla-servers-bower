cf44a22da304088ad63b50c791e62bc517dca124
Andrieu, Alban <alban.andrieu@free.fr>
2015-03-17 17:07:34 +0100

    tag version 1.0.1

--- publish.sh
+++ publish.sh
@@ -3,21 +3,20 @@
 cd $(dirname $0);
 SCRIPT_DIR=$(pwd)
 
-grunt
+grunt || exit 1
 export BUILD_NUMBER=1
-$SCRIPT_DIR/scripts/bower/publish.sh --action=prepare --git-push-dryrun=true --verbose=true
-#$SCRIPT_DIR/scripts/bower/publish.sh --action=publish --git-push-dryrun=true --verbose=true
+$SCRIPT_DIR/scripts/bower/publish.sh --action=prepare --git-push-dryrun=true --verbose=true || exit 2
+#$SCRIPT_DIR/scripts/bower/publish.sh --action=publish --git-push-dryrun=true --verbose=true || exit 3
 
 #cd $SCRIPT_DIR/scripts/bower/bower-sample-component
 #git push
 
-#bower register sample-component ssh://git@scm-git-eur.misys.global.ad:7999/risk/bower-fr-sample-component.git
-#bower register sample-component https://scm-git-eur.misys.global.ad/scm/risk/bower-fr-sample-component.git
+#bower register sample-component https://github.com/AlbanAndrieu/nabla-bower-sample-component.git
 #How to remove a wrong tag in git
-#git tag -d v0.0.0-local+sha.a6d3020
-#git push origin :refs/tags/v0.0.0-local+sha.a6d3020
+#git tag -d v1.0.0-build.1+sha.06a8c7e
+#git push origin :refs/tags/v1.0.0-build.1+sha.06a8c7e
 #create a tag
-#git tag 0.0.1-beta.1
-#git push origin tag 0.0.1-beta.1
+#git tag v1.0.1
+#git push origin tag v1.0.1
 #git tag
-git tag -l '0.0.*'
+git tag -l 'v1.0.*'
