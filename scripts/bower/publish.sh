34230305a38571690ce52a16ae518fec0d9af322
Andrieu, Alban <alban.andrieu@free.fr>
2015-06-09 17:56:41 +0200

    Disable remove jenkins tag

--- scripts/bower/publish.sh
+++ scripts/bower/publish.sh
@@ -16,10 +16,6 @@ function init {
   NEW_VERSION=$(cat $BUILD_DIR/version.txt)
   REPOS=(
     sample-component
-    auth
-    configuration
-    notification
-    header
   )
 }
 
@@ -105,10 +101,10 @@ function publish {
 
 	#remove the older tag created by jenkins
 	echo "-- Remove older bower-$repo tag"
-	git tag -l 'v*' | sort | head -1 | xargs git tag -d
-	#git fetch --prune --tags
-	git fetch
-	git tag -l 'v*' | sort | head -1 | xargs -n 1 git push --delete origin
+	#git tag -l 'v*' | sort | head -1 | xargs git tag -d
+	##git fetch --prune --tags
+	#git fetch
+	#git tag -l 'v*' | sort | head -1 | xargs -n 1 git push --delete origin
 
 	#git ls-remote --tags --heads ssh://git@github.com:AlbanAndrieu/nabla-bower-$repo.git | grep -o 'refs/tags/v[0-9]*\.[0-9]*\.[0-9]*-build.\w*+sha.\w*' | sort | head -20 | grep -o '[^\/]*$' | xargs git tag -d $1
 	#git ls-remote --tags --heads ssh://git@github.com:AlbanAndrieu/nabla-bower-$repo.git | grep -o 'refs/tags/v[0-9]*\.[0-9]*\.[0-9]*-build.\w*+sha.\w*' | sort | head -20 | grep -o '[^\/]*$' | xargs git push origin :$1
