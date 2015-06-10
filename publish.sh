9635d04121c07c3b3d801c786130d54a883993b0
Andrieu, Alban <alban.andrieu@free.fr>
2015-06-10 11:06:32 +0200

    Comment dry run

--- publish.sh
+++ publish.sh
@@ -9,10 +9,10 @@ rm -Rf $SCRIPT_DIR/scripts/bower/bower-*
 
 #grunt
 #export BUILD_NUMBER=1
-export DRY_RUN_OPT="--git-push-dryrun=true"
+#export DRY_RUN_OPT="--git-push-dryrun=true"
 $SCRIPT_DIR/scripts/bower/publish.sh --action=prepare --verbose=true
 $SCRIPT_DIR/scripts/bower/publish.sh --action=publish ${DRY_RUN_OPT} --verbose=true
 
 git tag -l 'v1.0.*'
 
-git ls-remote --tags --heads https://github.com/AlbanAndrieu/nabla-bower-sample-component.git | grep -o 'refs/tags/v[0-9]*\.[0-9]*\.[0-9]*' | sort -r | head | grep -o '[^\/]*$'
+#git ls-remote --tags --heads https://github.com/AlbanAndrieu/nabla-bower-sample-component.git | grep -o 'refs/tags/v[0-9]*\.[0-9]*\.[0-9]*' | sort -r | head | grep -o '[^\/]*$'
