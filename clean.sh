ce64edffc1e41d6f549ef325a21f51c59d252f8f
Andrieu, Alban <alban.andrieu@free.fr>
2015-11-24 16:31:28 +0100

    Do not clean on release

--- clean.sh
+++ clean.sh
@@ -11,6 +11,8 @@ SCRIPT_DIR=$(pwd)
 #grunt
 #export BUILD_NUMBER=1
 #export DRY_RUN_OPT="--git-push-dryrun=true"
-$SCRIPT_DIR/scripts/bower/clean.sh --action=clean-tag ${DRY_RUN_OPT} --verbose=true
+if [ "${IS_M2RELEASEBUILD}" = "false" ] ; then
+  $SCRIPT_DIR/scripts/bower/clean.sh --action=clean-tag ${DRY_RUN_OPT} --verbose=true
+fi
 
 git tag -l 'v0.0.*'
