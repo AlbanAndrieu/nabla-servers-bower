9f65e1d1ec3335626f7fdcdece7dabb2bd1388df
Alban Andrieu <alban.andrieu@free.fr>
2020-04-28 16:55:00 +0200

    Upgrade dependencies

--- build.sh
+++ build.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
-set -xv
+set -eu
 
+WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
+
+# shellcheck source=/dev/null
+source "${WORKING_DIR}/scripts/step-0-color.sh"
+
+#sudo apt install phantomjs
+#./node_modules/protractor/bin/webdriver-manager update --versions.chrome 2.37
+#npm install -g bower@1.8.4 grunt@1.0.3 grunt-cli@1.2.0 nsp@2.6.1 webdriver-manager@12.1.0
 #npm run update-webdriver
+#webdriver-manager update --chrome --versions.chrome=2.37
 
+echo -e "${magenta} grunt serve:dist --debug ${NC}"
+
+rm -f package-lock.json || true
 ./clean.sh
 
 ./mvnw install -Dserver=jetty9x
 
+echo -e "${green} ./mvnw clean install org.codehaus.cargo:cargo-maven2-plugin:run -Dserver=jetty9x ${NC}"
+
+npm list  > list.log
+
 #sudo npm install -g npm-license
-#npm-license || true
+npm-license || true
 
 #docker-compose -f docker-compose.yml -p TEST ps
 
--- clean.sh
+++ clean.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
-#set -xv
-
 # always stop on errors
 set -e
 
-rm -Rf .node_cache/ .node_tmp/  .tmp/ .bower/ bower_components/ node node_modules/ .sass-cache/ package-lock.json yarn.lock target/
+WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
+
+# shellcheck source=/dev/null
+source "${WORKING_DIR}/scripts/step-0-color.sh"
+
+echo -e "${magenta} Cleaning started. ${NC}"
+
+rm -Rf npm/ .node_cache/ .node_tmp/ .tmp/ .bower/ bower_components/ node/ node_modules/ .sass-cache/ .scannerwork/ .repository/ target/ target-eclipse/ build/ phantomas/ dist/ docs/groovydocs/ docs/js/ docs/partials/ site/ coverage/
 
 #source $(dirname $0)/scripts/utils.inc
-cd $(dirname $0);
-SCRIPT_DIR=$(pwd)
-#rm -Rf $SCRIPT_DIR/scripts/bower/bower-*
+#rm -Rf $WORKING_DIR/scripts/bower/bower-*
 
 #grunt
 #export BUILD_NUMBER=1
 #export DRY_RUN_OPT="--git-push-dryrun=true"
 if [ "${IS_M2RELEASEBUILD}" = "false" ] ; then
-  $SCRIPT_DIR/scripts/bower/clean.sh --action=clean-tag ${DRY_RUN_OPT} --verbose=true
+  $WORKING_DIR/scripts/bower/clean.sh --action=clean-tag ${DRY_RUN_OPT} --verbose=true
 fi
 
 git tag -l 'v0.0.*'
+
+rm -f checkstyle.xml package-lock.json
+
+echo -e "${magenta} NPM cleaning started. ${NC}"
+
+npm --version
+npm cache clean || true
+npm cache verify || true
+
+echo -e "${green} Cleaning DONE. ${NC}"
+
+exit 0
